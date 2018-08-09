3a15a0a995d6202d55fa353b85c8b30d009a8a96
songzhw <songzhw2012@gmail.com>
2018-08-09 00:01:50 -0400

    pass files back

--- backend/SixMall/server.js
+++ backend/SixMall/server.js
@@ -3,28 +3,31 @@ var url = require('url')
 var queryString = require('querystring')
 var fs = require('fs')
 
-http.createServer(onRequest).listen(8899, function(){
+http.createServer(onRequest).listen(8899, function () {
       console.log("server starts up! (listen on 8899)")
 })
 
-/*
+/* 应对两种请求: 一种是静态文件(如图片)的请求, 返回文件; 另一种是API请求, 返回json串:
+
 a.com/123.jpg
 
 a.com/home
 a.com/item?id=123&from=3rd
 */
-function onRequest(req, resp){
-      var reqUrl = req.url
-
+function onRequest(req, resp) {
+      var reqUrl = req.url          //=> "item?id=2&from=a" , "/", "/public/a.jpg"
+      var apiName = url.parse(reqUrl).pathname  //=> "/item", "/", "/public/a.jpg"
+      console.log("szw : url = " + reqUrl + " ; api = " + apiName)
 }
 
-function send404(response, err) {
-      response.writeHead(404, { 'Content-Type': 'application/json' });
-      response.write('Error 404: resource not found : ' + err);
-      response.end();
-}
 
-function sendFile(response, filePath, fileContents) {
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end(fileContents);
+
+/* response : 
+{
+      "code":100 //100为成功
+      "msg": "success"
+      "payload":{
+            ...
+      }
 }
+*/
---
+++ backend/SixMall/splash/splash.js
@@ -0,0 +1,39 @@
+var fs = require('fs')
+
+// TODO 后续splash不直接返回图片, 改为返回图片url, 让client端自己去加载图片
+
+function onRequest(resp){
+      console.log("splash API : get request")
+
+      var randomNum = Math.floor( 1+ Math.random() * 7)
+      var path = "../public/splash"+randomNum+".jpg"
+
+      fs.exists(path, function(isExist){
+            if(!isExist){
+                  sendError(resp, 200, "file ["+path+"] not found")
+                  return
+            } 
+            fs.readFile(path, function(err, data){
+                  if(err){
+                        sendError(resp, 210, "read file error : "+path)
+                        return
+                  }
+                  sendFile(resp, data)
+            })
+      })
+
+}
+
+function sendError(response, errCode, errMsg) {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.write('{"code": '+errCode+', "msg":"'+errMsg+',"payload":{}}');
+      response.end();
+}
+
+
+function sendFile(response, fileContents) {
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.end(fileContents);
+}
+
+
