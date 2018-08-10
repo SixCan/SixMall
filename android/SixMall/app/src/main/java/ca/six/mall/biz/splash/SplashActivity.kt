cc2be7b74e42b1c4106feec7f9f4244c69c77cd3
songzhw <songzhw2012@gmail.com>
2018-08-10 13:03:18 -0400

    databinding - success!!!

--- android/SixMall/app/src/main/java/ca/six/mall/biz/splash/SplashActivity.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/biz/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package ca.six.mall.biz.splash
 
 import android.annotation.SuppressLint
 import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
@@ -17,15 +18,14 @@ class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
-        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        val binding = DataBindingUtil.setContentView<ActivitySplashBinding>(this, R.layout.activity_splash)
         val viewModel : SplashViewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
-        binding.vm = viewModel
+        binding?.vm = viewModel
 
         viewModel.init()
 
-        println("szw:${binding.tvSplash.text}")
+        println("szw:${binding?.tvSplash?.text}")
         println("szw:${viewModel.desp.get()}")
         // TODO rever it back
 //        handler.sendEmptyMessageDelayed(ACTION_ID_SPLASH_DONE, DURATION_SPLASH)
