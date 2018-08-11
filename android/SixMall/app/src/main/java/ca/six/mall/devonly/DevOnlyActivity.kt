839ef437d0f3a48608ed823305df6aa554457567
songzhw <songzhw2012@gmail.com>
2018-08-11 16:55:42 -0400

    android: planet view

--- android/SixMall/app/src/main/java/ca/six/mall/devonly/DevOnlyActivity.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/devonly/DevOnlyActivity.kt
@@ -1,14 +1,19 @@
 package ca.six.mall.devonly
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import ca.six.mall.R
+import kotlinx.android.synthetic.main.activity_dev_only.*
 
 class DevOnlyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dev_only)
+
+        testView.initData(Color.GREEN, R.drawable.panda, "Panda")
+
     }
 
 }
--- android/SixMall/app/src/main/java/ca/six/mall/view/solar/PlanetView.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/view/solar/PlanetView.kt
@@ -1,16 +1,40 @@
 package ca.six.mall.view.solar
 
 import android.content.Context
-import android.graphics.*
-import android.graphics.Bitmap.Config
-import android.graphics.PorterDuff.Mode
+import android.graphics.Color
+import android.support.annotation.ColorInt
+import android.support.annotation.DrawableRes
 import android.util.AttributeSet
-import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
+import ca.six.mall.view.CircleCornerImageView
 
 
 class PlanetView @JvmOverloads constructor(context: Context, attr: AttributeSet? = null, defStyle: Int = 0)
     : LinearLayout(context, attr, defStyle) {
+
+    init {
+        orientation = VERTICAL
+    }
+
+    fun initData(bgColor : Int = Color.TRANSPARENT, iconRes : Int = 0, text : String = ""){
+        val circleView = CircleCornerImageView(context)
+        circleView.setStyleAsCircle()
+        circleView.setBackgroundColor(bgColor)
+        circleView.setImageResource(iconRes)
+
+        val textView = TextView(context)
+        textView.setText(text)
+
+        this.addView(circleView)
+        this.addView(textView)
+        invalidate()
+    }
+
+
+
+
+
 }
 
 
