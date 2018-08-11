04d4c7e61467c9daf7e957fac9d257100d4ccb11
songzhw <songzhw2012@gmail.com>
2018-08-11 16:41:08 -0400

    add circle round imagview + Circle imageview

--- android/SixMall/app/src/main/java/ca/six/mall/view/CircleCornerImageView.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/view/CircleCornerImageView.kt
@@ -10,18 +10,32 @@ class CircleCornerImageView  @JvmOverloads constructor(context: Context, attr: A
     val clipPath = Path()
     val drawFilter = PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
     lateinit var roundRect : RectF
+    var roundRx : Float = 90.0f
+    var roundRy : Float = 90.0f
+    var thisWidth: Int = 0
+    var thisHeight: Int = 0
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        thisWidth = w
+        thisHeight = h
         roundRect = RectF(0f, 0f, w.toFloat(), h.toFloat())
     }
 
     override fun onDraw(canvas: Canvas) {
         canvas.setDrawFilter(drawFilter)  //抗锯齿
 
-        clipPath.addRoundRect(roundRect, 90.0f, 90.0f, Path.Direction.CW)
+        clipPath.addRoundRect(roundRect, roundRx, roundRy, Path.Direction.CW)
         canvas.clipPath(clipPath)
 
         super.onDraw(canvas)
+    }
+
+
+    // not just a circle corner, the image itself is a CircleImageView
+    fun setStyleAsCircle(){
+        roundRx = 1.0f * thisWidth / 2
+        roundRy = 1.0f * thisHeight / 2
+        invalidate()
     }
 }
