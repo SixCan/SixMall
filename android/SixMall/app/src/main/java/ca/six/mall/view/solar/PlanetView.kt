2eb467db865941129189fb4e4771c93151100423
songzhw <songzhw2012@gmail.com>
2018-08-11 21:30:28 -0400

    android: PlanetView - measure

--- android/SixMall/app/src/main/java/ca/six/mall/view/solar/PlanetView.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/view/solar/PlanetView.kt
@@ -12,31 +12,32 @@ import ca.six.mall.view.CircleCornerImageView
 
 class PlanetView @JvmOverloads constructor(context: Context, attr: AttributeSet? = null, defStyle: Int = 0)
     : LinearLayout(context, attr, defStyle) {
-    var ivHeight  = 0
-    var tvHeight  = 0
-    var thisWidth  = 0
+    var bgColor: Int = Color.TRANSPARENT
+    var iconRes: Int = 0
+    var text: String = ""
 
     init {
         orientation = VERTICAL
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        thisWidth = MeasureSpec.getSize(widthMeasureSpec)
-        val thisHeight = MeasureSpec.getSize(heightMeasureSpec)
-
-        ivHeight = (thisHeight * 0.8f).toInt()
-        tvHeight = thisHeight - ivHeight
-
-        println("szw thisWidth = $thisWidth ; thisHeight = $thisHeight")
-        println("szw ivHeight = $ivHeight ; tvHeight = $tvHeight")
-
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        println("szw onFinishInflate()")
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        println("szw onMeasure()")
+    }
 
-    fun initData(bgColor: Int = Color.TRANSPARENT, iconRes: Int = 0, text: String = "") {
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        println("szw onSizeChanged()")
+
+        val ivHeight = (h * 0.8f).toInt()
+        val tvHeight = h - ivHeight
+
         val circleView = ImageView(context)
-//        circleView.setStyleAsCircle()
         circleView.setBackgroundColor(bgColor)
         circleView.setImageResource(iconRes)
 
@@ -48,7 +49,29 @@ class PlanetView @JvmOverloads constructor(context: Context, attr: AttributeSet?
         //addView()自己会调用 requestLayout() 与 invalidate(true);
         println("szw --------- addView ----------- $ivHeight, $tvHeight")
         this.addView(circleView, LinearLayout.LayoutParams(ivHeight, ivHeight))
-        this.addView(textView, LinearLayout.LayoutParams(thisWidth, tvHeight))
+        this.addView(textView, LinearLayout.LayoutParams(w, tvHeight))
+
+    }
+
+
+    fun initData(bgColor: Int = Color.TRANSPARENT, iconRes: Int = 0, text: String = "") {
+        this.bgColor = bgColor
+        this.iconRes = iconRes
+        this.text = text
+        println("szw initData()")
+
+        val circleView = ImageView(context)
+        circleView.setBackgroundColor(bgColor)
+        circleView.setImageResource(iconRes)
+
+        val textView = TextView(context)
+        textView.setText(text)
+        textView.setTextColor(Color.BLACK)
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 22.0f)
+
+        // addView()自己会调用 requestLayout() 与 invalidate(true);
+        this.addView(circleView)
+        this.addView(textView)
     }
 
 
