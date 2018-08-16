0f50283fc9bf60b3d13691332d264fc2dd8fbebe
songzhw <songzhw2012@gmail.com>
2018-08-16 17:24:09 -0400

    Rv : OneAdapter + Databinding

--- android/SixMall/app/src/main/java/ca/six/mall/devonly/TempAdapter.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/devonly/TempAdapter.kt
@@ -1,19 +1,29 @@
 package ca.six.mall.devonly
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView.Adapter
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import ca.six.mall.view.rv.RvViewHolder
+import ca.six.mall.view.rv.one_binding.RvBindingViewHolder
 
-class TempAdapter : Adapter<RvViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class TempAdapter<T>(var layoutResId: Int, var bindingVariable: Int, var data: List<T>? = null)
+    : Adapter<RvBindingViewHolder>() {
+    var inflater : LayoutInflater? = null
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return if(data == null) 0 else data!!.size
     }
 
-    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvBindingViewHolder {
+        inflater = inflater ?: LayoutInflater.from(parent.context)
+        val binding : ViewDataBinding = DataBindingUtil.inflate(inflater!!, layoutResId, parent, false)
+        return RvBindingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RvBindingViewHolder, position: Int) {
+        val item = data!!.get(position)
+        holder.binding.setVariable(bindingVariable, item)
     }
 }
--- android/SixMall/app/src/main/java/ca/six/mall/view/rv/one_binding/RvBindingViewHolder.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/view/rv/one_binding/RvBindingViewHolder.kt
@@ -2,14 +2,7 @@ package ca.six.mall.view.rv.one_binding
 
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
-import ca.six.mall.BR
 
-
-class RvBindingViewHolder(private val binding: ViewDataBinding)
+class RvBindingViewHolder(var binding: ViewDataBinding)
     : RecyclerView.ViewHolder(binding.root) {
-
-    fun bind(obj: Any) {
-        binding.setVariable(BR.obj, obj)
-        binding.executePendingBindings()
-    }
 }
