90e7b94514d1c230536e99b1e1f579f130c489aa
songzhw <songzhw2012@gmail.com>
2018-08-17 11:15:58 -0400

    Rv : OneTypesAdapter + Databinding crash : ClassCastException: ca.six.mall.devonly.pojo.DevOnlyPerson cannot be cast to ca.six.mall.devonly.pojo.DevOnlyUser

--- android/SixMall/app/src/main/java/ca/six/mall/util/DataBindingEx.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/util/DataBindingEx.kt
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView
 import android.widget.ImageView
 import ca.six.mall.view.rv.one_binding.BindingRow
 import ca.six.mall.view.rv.one_binding.OneBindingAdapter
+import ca.six.mall.view.rv.one_binding_types.BindingTypesRow
+import ca.six.mall.view.rv.one_binding_types.OneBindingTypesAdapter
 import com.squareup.picasso.Picasso
 
 object DataBindingEx {
@@ -25,6 +27,16 @@ object DataBindingEx {
     fun <T> rvSetData(rv: RecyclerView, data: List<T>?, row : BindingRow?) {
         if (row != null) {
             val adapter = OneBindingAdapter<T>(row.layoutResId, row.bindingId, data)
+            rv.adapter = adapter
+        }
+    }
+
+    @BindingAdapter( "rows")
+    @JvmStatic
+    @MainThread
+    fun rvSetRows(rv: RecyclerView, rows :  List<BindingTypesRow<*>>?) {
+        if (rows != null){
+            val adapter = OneBindingTypesAdapter(rows)
             rv.adapter = adapter
         }
     }
