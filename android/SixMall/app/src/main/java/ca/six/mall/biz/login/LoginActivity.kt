05d9ecf942d7a3ba8738c79d2f5e2ad49872792c
songzhw <songzhw2012@gmail.com>
2018-08-20 11:25:52 -0400

    android: create a circle avatar for login page

--- android/SixMall/app/src/main/java/ca/six/mall/biz/login/LoginActivity.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/biz/login/LoginActivity.kt
@@ -1,13 +1,26 @@
 package ca.six.mall.biz.login
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import ca.six.mall.R
 import ca.six.mall.core.BaseActivity
+import ca.six.mall.databinding.ActivityLoginBinding
+import ca.six.mall.util.dp2px
+import ca.six.tomato.util.getCircleBitmap
+import ca.six.tomato.util.getVectorBitmap
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        val binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login)
+        binding.setLifecycleOwner(this)
+
+        val avatarSize = this.dp2px(60f).toInt()
+        val originalAvata = getVectorBitmap(this, R.drawable.ic_avatar, avatarSize, avatarSize)
+        val circleAvatar = getCircleBitmap(originalAvata, originalAvata.width / 2f)
+        binding.ivAvatar.setImageBitmap(circleAvatar)
+        originalAvata.recycle()
     }
 }
