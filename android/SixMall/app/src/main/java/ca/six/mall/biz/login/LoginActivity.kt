f5578d5256b47006cdbaa025550f683dc9dabb03
songzhw <songzhw2012@gmail.com>
2018-08-20 10:51:06 -0400

    login activity

---
+++ android/SixMall/app/src/main/java/ca/six/mall/biz/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package ca.six.mall.biz.login
+
+import android.os.Bundle
+import ca.six.mall.R
+import ca.six.mall.core.BaseActivity
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
