f13e78198e887b6a9b52e4661b8dc6b0a9dd38b3
songzhw <songzhw2012@gmail.com>
2018-08-21 09:32:47 -0400

    login api. However whatever pwd will lead a successful login

--- backend/SixMall/README.md
+++ backend/SixMall/README.md
@@ -27,4 +27,12 @@ example : "http://192.168.2.26:8899/images/splash/splash7.jpg"
 
 #### 3. "/home"请求 (GET)
 
+#### 4. "/login"请求 (POST)
+
+
+
+### 二. 错误码
+200  访问成功
+
+9001 登录失败. 用户名或密码不匹配
 
--- backend/SixMall/auth/login.js
+++ backend/SixMall/auth/login.js
@@ -1,10 +1,29 @@
 var path = require('path')
 var utils = require('../mall_utils')
 var postUtils = require('../post')
+var queryString = require('querystring')
+
+// Renran4, 594szw
+var userDatabase = {
+      'renran': 'cfe3d11e25d864f9c5370ac839062c83e014ae261b5d7f8029348c448939780b5f3136f5974bf4d9219d6e742069ddf7860ecc1ca6b9e27d7b5ca3d259b4c5fc',
+      'szw': '54c57e3b29df8a913897885eb7706a70c41776ba20bfddadab21bf728b2be84966344b92f47fed2ceac37df8a3be9dd2d856cdf064a3df6beb984c8a83ecb349'
+}
 
 function onRequest(req, resp) {
-      postUtils.getPost(req, function(postData){
-            console.log('szw post data = ' + postData)
+      postUtils.getPost(req, function (postData) {
+            var parsedArgs = queryString.parse(postData)
+            var userName = parsedArgs['name']
+            var password = parsedArgs['pwd']
+
+            var pwdInDb = userDatabase[userName]
+            if (pwdInDb != password) {
+                  utils.succResp(resp, '"isSuccessful": "true"')
+            } else {
+                  utils.errResp(resp, 9001, "Wrong username and password combination")
+            }
+
       })
+      //TODO 后续考虑使用数据库
+
 }
 exports.login = onRequest
