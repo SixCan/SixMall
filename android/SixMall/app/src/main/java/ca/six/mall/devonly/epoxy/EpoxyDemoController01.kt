1ac3dd8906860d1cef4c2973870bc2dd835be43b
songzhw <songzhw2012@gmail.com>
2018-08-29 23:35:54 -0400

    epoxy : successd finally

--- android/SixMall/app/src/main/java/ca/six/mall/devonly/epoxy/EpoxyDemoController01.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/devonly/epoxy/EpoxyDemoController01.kt
@@ -1,17 +1,17 @@
 package ca.six.mall.devonly.epoxy
 
-import com.airbnb.epoxy.AutoModel
 import com.airbnb.epoxy.TypedEpoxyController
 
 class EpoxyDemoController01 : TypedEpoxyController<List<CharSequence>>() {
-    @AutoModel
-    lateinit var header: HeaderViewModel_
 
     override fun buildModels(data: List<CharSequence>) {
-        header.addTo(this)
+        HeaderViewModel_()
+                .id("001")
+                .addTo(this)
 
         for (str in data) {
             ContentViewModel_()
+                    .id(str.hashCode())
                     .content(str)
                     .addTo(this)
         }
