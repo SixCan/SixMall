7c705506ae167c0e8fda89b092162420cad23e4a
songzhw <songzhw2012@gmail.com>
2018-08-30 17:39:26 -0400

    DevOnlyHeaderBindingBindingModel_ seems not working. Maybe it's because the databinding.v2=false?

--- android/SixMall/app/src/main/java/ca/six/mall/devonly/epoxy/binding/EpoxyBindingController.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/devonly/epoxy/binding/EpoxyBindingController.kt
@@ -1,4 +1,26 @@
 package ca.six.mall.devonly.epoxy.binding
 
-class EpoxyBindingController {
+import ca.six.mall.DevOnlyContentBindingBindingModel_
+import ca.six.mall.DevOnlyHeaderBindingBindingModel_
+import ca.six.mall.devOnlyHeaderBinding
+import com.airbnb.epoxy.Typed2EpoxyController
+
+class EpoxyBindingController : Typed2EpoxyController<Int, List<String>>(){
+
+
+    override fun buildModels(data1: Int, data2: List<String>) {
+        DevOnlyHeaderBindingBindingModel_()
+                .id(103)
+                .imageResId(data1)
+//                .click
+                .addTo(this)
+
+        for (str in data2) {
+            DevOnlyContentBindingBindingModel_()
+                    .id(str.hashCode())
+                    .itemString(str)
+                    .addTo(this)
+        }
+    }
+
 }
--- android/SixMall/app/src/main/java/ca/six/mall/devonly/epoxy/binding/ExposyDemo03.kt
+++ android/SixMall/app/src/main/java/ca/six/mall/devonly/epoxy/binding/ExposyDemo03.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import ca.six.mall.R
+import ca.six.mall.devonly.epoxy.viewholder.EpoxyVhController
 import kotlinx.android.synthetic.main.activity_dev_only_epoxy_rv_one.*
 
 class ExposyDemo03 : AppCompatActivity() {
@@ -12,5 +13,11 @@ class ExposyDemo03 : AppCompatActivity() {
         setContentView(R.layout.activity_dev_only_epoxy_rv_one)
 
         rvEpoxy.layoutManager = LinearLayoutManager(this)
+
+        val data = List(13) { i -> "Binding Item $i" }
+        val controller = EpoxyBindingController()
+        controller.setData(R.drawable.dev_only_westlake, data)
+
+        rvEpoxy.setController(controller)
     }
 }
