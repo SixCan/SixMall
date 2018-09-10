8602af46bd7de9af0149bb9abecf413a18a9a82f
songzhw <songzhw2012@gmail.com>
2018-09-09 22:41:23 -0400

    test Home page: Espresso seems works wel with DataBinding right now

--- android/SixMall/app/src/androidTest/java/ca/six/mall/biz/home/HomeTest.kt
+++ android/SixMall/app/src/androidTest/java/ca/six/mall/biz/home/HomeTest.kt
@@ -1,25 +1,18 @@
 package ca.six.mall.biz.home
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.IdlingPolicies
-import android.support.test.espresso.IdlingRegistry
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.withHint
-import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import ca.six.mall.R
-import ca.six.mall.core.AsyncIdlingRes
-import ca.six.mall.core.IIdlingFlag
 import ca.six.mall.core.http.HttpEngine
-import ca.six.mall.util.SleepIdler
 import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.concurrent.TimeUnit
 
 
 @RunWith(AndroidJUnit4::class)
@@ -30,26 +23,17 @@ class HomeTest {
     @JvmField    //@get:Rule
     var activityRule = ActivityTestRule<HomeActivity>(HomeActivity::class.java)
 
-//    @Test
-//    fun init_showSolarMenu() {
-//        initEnv()
-//        onView(withId(R.id.menuMyAccount))
-//                .check(matches(isDisplayed()))
-//    }
+    @Test
+    fun init_showSolarMenu() {
+        initEnv()
+        onView(withId(R.id.menuMyAccount))
+                .check(matches(isDisplayed()))
+    }
 
     @Test
     fun init_getHintFromServer() {
-        val activity = activityRule.activity
-        val idle = SleepIdler("HomeHint", 3000)
-        IdlingPolicies.setMasterPolicyTimeout(6, TimeUnit.SECONDS)
-        IdlingPolicies.setIdlingResourceTimeout(10, TimeUnit.SECONDS)
-        IdlingRegistry.getInstance().register(idle)
-
         onView(withId(R.id.etSearch))
                 .check(matches(withHint("iphone")))
-        val hint = activityRule.activity.viewModel.keyWordHint
-
-        IdlingRegistry.getInstance().unregister(idle)
     }
 
     companion object {
