d6abcd455b23144fb96cb6d16b1cc2d517b24416
songzhw <songzhw2012@gmail.com>
2018-09-09 22:51:33 -0400

    mockito + kotlin : when

--- android/SixMall/app/src/androidTest/java/ca/six/mall/biz/home/HomeTest.kt
+++ android/SixMall/app/src/androidTest/java/ca/six/mall/biz/home/HomeTest.kt
@@ -8,12 +8,12 @@ import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import ca.six.mall.R
 import ca.six.mall.core.http.HttpEngine
-import org.junit.AfterClass
-import org.junit.BeforeClass
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
+data class Apple(val name: String){}
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -22,6 +22,12 @@ class HomeTest {
     @Rule
     @JvmField    //@get:Rule
     var activityRule = ActivityTestRule<HomeActivity>(HomeActivity::class.java)
+
+    @Before
+    fun setUp() {
+        val apple = mock(Apple::class.java)
+        `when`(apple.name).thenReturn("ios")
+    }
 
     @Test
     fun init_showSolarMenu() {
@@ -70,7 +76,6 @@ class HomeTest {
         @JvmStatic
         fun reset() {
             HttpEngine.isMock = false
-            println("szw Test @AfterClass : ${HttpEngine.isMock}")
         }
 
     } // end of companion object
