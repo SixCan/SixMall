14d8fddca0231dac91085dcc2dc3f4c06759bb38
songzhw <songzhw2012@gmail.com>
2018-09-17 10:23:30 -0400

    espresso test RecyclerView

--- android/SixMall/app/src/androidTest/java/ca/six/mall/biz/home/HomeTest.kt
+++ android/SixMall/app/src/androidTest/java/ca/six/mall/biz/home/HomeTest.kt
@@ -2,12 +2,15 @@ package ca.six.mall.biz.home
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView
 import ca.six.mall.R
 import ca.six.mall.core.http.HttpEngine
+import ca.six.mall.view.rv.RecyclerViewMatcher
 import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.Rule
@@ -35,6 +38,19 @@ class HomeTest {
                 .check(matches(withHint("iphone")))
     }
 
+    @Test
+    fun init_getRecommendationsFromServer() {
+        val recommendationPosition = 3 //0: solar menu; 1: title; 2,3 : recommendation
+        onView(withId(R.id.rvHome))
+                .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(recommendationPosition))
+
+
+        val rvItem1 = RecyclerViewMatcher(R.id.rvHome).atPosition(recommendationPosition)
+        onView(rvItem1)
+                .check(matches(withText("Cat")))
+    }
+
+
     companion object {
         @BeforeClass
         @JvmStatic
@@ -56,7 +72,7 @@ class HomeTest {
       {
         "id": "33880014",
         "pic": "http://192.168.2.26:8899/images/items/cat4.webp",
-        "title": "Kitty Adoption"
+        "title": "Cat Adoption"
       }
     ]
   }
